b53807f3c311550586fb909dfb6919010ac8c8f0
julomontiti <julomontiti@gmail.com>
2018-10-02 17:15:31 +0200

    Premier pas vers l'autoload

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
 <?php
-        require __DIR__ . '/../src/Controller/ItemController.php';
-    ?>
+/**
+ * Created by PhpStorm.
+ * User: wilder13
+ * Date: 01/10/18
+ * Time: 18:41
+ */
+
+
+use App\Wcs\Hello;
+use HelloWorld\SayHello;
+
+require '../vendor/autoload.php';
+
+
+$julo = new Hello();
+
+echo $julo->talk();
+
+$juju = new SayHello();
+
+echo $juju->world();
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder13
+ * Date: 01/10/18
+ * Time: 18:42
+ */
+
+namespace App\Wcs;
+
+class Hello
+{
+    /**
+     * @return string
+     */
+    public function talk()
+   {
+       return "Hello World";
+   }
+
+
+
+
+}
